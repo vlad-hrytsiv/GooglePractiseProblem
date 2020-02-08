f10ad875a9bc2a0d728efcface15126d9933e759
vlad-hrytsiv <54137412+vlad-hrytsiv@users.noreply.github.com>
2020-02-08 18:32:58 +0200

    Додав таймер виконання коду

--- MorePizza/MorePizza/Program.cs
+++ MorePizza/MorePizza/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,8 +33,11 @@ namespace MorePizza
         }
         static void Main(string[] args)
         {
-            int[] pizzaSlices = new int[4] { 2, 5, 6, 8 }; 
-            int numberOfParticipants = 17;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int[] pizzaSlices = new int[50] { 7, 12, 12, 13, 14, 28, 29, 29, 30, 32, 32, 34, 41, 45, 46, 56, 61, 61, 62 ,63 ,65,68 ,76 ,77 ,77 , 92, 93 ,94, 97, 103 ,113, 114, 114, 120, 135 ,145 ,145 ,149 ,156, 157, 160 ,169 ,172 ,179, 184, 185 ,189, 194, 195, 195 }; 
+            int numberOfParticipants = 4500;
 
             Program program = new Program();
 
@@ -47,6 +52,15 @@ namespace MorePizza
             Console.WriteLine();*/
 
             Console.Write("Answer: "+program.getDynamicTable(pizzaSlices, numberOfParticipants)[numberOfParticipants, pizzaSlices.Length]+"\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 1);
+            Console.WriteLine("RunTime " + elapsedTime);
         }
     }
 }
